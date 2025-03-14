4037ea53d4cf7a4455272c5f8d78088ad2de8e10
Vi. <131671554+mzzvxm@users.noreply.github.com>
2025-03-14 20:19:57 -0300

    Update questionSpoof.js

--- Plugins/questionSpoof.js
+++ Plugins/questionSpoof.js
@@ -1,6 +1,6 @@
 const originalFetch = window.fetch;
 
-window.fetch = async function (input, init) {
+window.fetch = async function(input, init) {
     let requestBody;
     if (input instanceof Request) requestBody = await input.clone().text();
     else if (init?.body) requestBody = init.body;
@@ -15,7 +15,7 @@ window.fetch = async function (input, init) {
             let questionData = JSON.parse(responseData.data.assessmentItem.item.itemData);
             if (questionData.question.content[0] === questionData.question.content[0].toUpperCase()) {
                 questionData.answerArea = { "calculator": false, "chi2Table": false, "periodicTable": false, "tTable": false, "zTable": false };
-                questionData.question.content = " " + `[[☃ radio 1]]`;
+                questionData.question.content = " " + [[☃ radio 1]];
                 questionData.question.widgets = { "radio 1": { options: { choices: [ { content: "✅┃khanbypass by @mzzvxm!", correct: true } ] } } };
                 responseData.data.assessmentItem.item.itemData = JSON.stringify(questionData);
                 return new Response(JSON.stringify(responseData), { status: response.status, statusText: response.statusText, headers: response.headers });
